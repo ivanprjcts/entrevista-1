4e44c103a7990ea4fd0434248cd02e84811e6154
Iván Martín Vedriel <ivan.martinvedriel@telefonica.com>
2020-09-15 07:58:28 +0200

    sample code

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+
+def sample(name):
+    i = 0
+    elements = [5, 1, 0, 8]
+    while i < 4:
+        if elements[i]:
+            print(i)
+            print elements[i]
+
+        i = i + 1
+
+
+if __name__ == '__main__':
+    sample('MyCode')
